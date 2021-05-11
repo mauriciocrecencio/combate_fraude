42e2efe30c25bc14914061b7e7d61004911caf06
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2021-05-11 15:19:58 -0300

    add option to connect to cloud database

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,4 +1,15 @@
 import mongoose from 'mongoose'
 
-const url = `mongodb+srv://combate:combate@cluster0.5gdtx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false })
+const MONGO_HOSTNAME = '127.0.0.1'
+const MONGO_PORT = '27017'
+const MONGO_DB = 'combate_a_fraude'
+
+const cloudMongoDB = `mongodb+srv://combate:combate@cluster0.5gdtx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const localMongoDB = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`
+
+// If you want to use my cloud database, please replace localMongoDB to cloudMongoDB in the next line
+mongoose.connect(localMongoDB, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+})
